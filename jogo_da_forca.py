b6fcf94eecf3baccb043ed7e1fcf346f4c3ce16e
Leonardo Carvalho <leonardobang123carvalho@gmail.com>
2022-08-11 21:33:49 -0400

    Adicionando e corrigindo mecanica de sorteio de palavras, nivel e rotfix

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -5,146 +5,171 @@ import os
 from tkinter.tix import Tree
 from turtle import back
 
-# Variavis
-banco_de_palavras = {
-    'facil': {
-        1: ('Sentimento', 'amor'),
-        2: ('Materia', 'fisica'),
-        3: ('Objeto', 'lapis'),
-    },
-    'medio': {
-        1: ('Animal', 'tucano'),
-        2: ('Pais', 'argentina'),
-        3: ('Objeto', 'compasso'),
-    },
-    'dificil': {
-        1: ('Animal', 'salamandra '),
-        2: ('Profisão', 'diretor-geral'),
-        3: ('Objeto', 'Zarabatana'),
-    }
-}
-banco_de_palavras_copia = banco_de_palavras.copy()
-palavra_sorteada = 0
-dificuldade = ''
-opcao = 0
-
-
-def sorteador_palavra():
-    global palavra_sorteada, banco_de_palavras_copia
-    palavra_sorteada = random.randint(
-        1, len(banco_de_palavras_copia[dificuldade]))
-    print(palavra_sorteada)
-    palavra = banco_de_palavras_copia[dificuldade][palavra_sorteada]
-    return palavra
-
-
-# Escolhendo Dificuldade
-while True:
-    os.system('cls')
-    print('='*55)
-    print('Escolha a dificildade:')
-    print('1-Facil')
-    print('2-Medio')
-    print('3-Dificil')
-    print('='*55)
-    opcao = input('Escolha: ')
-    if opcao == '1':
-        dificuldade = 'facil'
-        break
-    elif opcao == '2':
-        dificuldade = 'medio'
-        break
-    elif opcao == '3':
-        dificuldade = 'dificil'
-        break
-    else:
-        os.system('cls')
-        print('='*55)
-        print('Digite um valor valido!!!')
-        print('='*55)
-        time.sleep(2)
+pontos = 0
+nivel = 0
 # Loop principal
 while True:
+    # Variavis
+    banco_de_palavras = {
+        'facil': [
+            ('Sentimento', 'amor'),
+            ('Materia', 'fisica'),
+            ('Objeto', 'lapis'),
+        ],
+        'medio': [
+            ('Animal', 'tucano'),
+            ('Pais', 'argentina'),
+            ('Objeto', 'compasso'),
+        ],
+        'dificil': [
+            ('Animal', 'salamandra'),
+            ('Profisão', 'diretor-geral'),
+            ('Objeto', 'Zarabatana'),
+        ]
+    }
+    banco_de_palavras_copia = banco_de_palavras.copy()
+    palavra_sorteada = 0
+    dificuldade = ''
+    opcao = 0
 
-    # Variaveis game
-    game_over = False
-    tentativas = 6
-    alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    palavra = ''
-    acerto = False
-    palavra_secreta = []
-
-    os.system('cls')
-
-    palavra = sorteador_palavra()
-
-    # Criando a palavra_secreta com a palavra
-    for letraP in range(0, len(palavra[1])):
-        if palavra[1][letraP] == ' ':
-            palavra_secreta.append(' ')
-        if palavra[1][letraP] == '-':
-            palavra_secreta.append('-')
+    def sorteador_palavra():
+        global palavra_sorteada, banco_de_palavras_copia
+        palavra_sorteada = random.randint(
+            0, len(banco_de_palavras_copia[dificuldade]) - 1)
+        print(palavra_sorteada)
+        if len(banco_de_palavras_copia[dificuldade]) > 0:
+            palavra = banco_de_palavras_copia[dificuldade][palavra_sorteada]
         else:
-            palavra_secreta.append('_')
+            palavra = 'game_over'
+        return palavra
 
-    # Loop game
-    while game_over == 0:
+    # Escolhendo Dificuldade
+    while True:
+        if pontos >= 2:
+            nivel += 1
+        pontos = 0
+
         os.system('cls')
-
-        # Tela game
-        print(f'Tentativas: {tentativas}')
-        print(f'Tema: {palavra[0]}')
         print('='*55)
-        print(str(''.join(palavra_secreta)))
+        print('-'*55)
+        print('JOGO DA FORCA')
+
+        print('-'*55)
         print('='*55)
-        print(str(' '.join(alfabeto)))
+        print('Entre em cada nivel e consiga 3 vitórias')
         print('='*55)
-        letra = input('Digite uma letra: ')
-
-        # Verificação de validade do valor digitado na variavel letra
-        if len(letra) == 1:
-            acerto = False
-            # Verificando se a letra digitada esta na palavra
-            for a in range(0, len(palavra[1])):
-                if palavra[1][a] == letra:
-                    # Colocando a letra na palavra_secreta
-                    palavra_secreta[a] = letra
-                    acerto = True
-
-            # Verificando se o usuario acertou a letra
-            if acerto == False and letra in alfabeto:
-                tentativas -= 1
-
-            # Verificando se a letra esta no alfabeto dispoivel
-            if letra in alfabeto:
-                # Removendo letra do alfabeto
-                alfabeto.remove(letra)
-
-            # Verificando vitoria!
-            if palavra[1] == str(''.join(palavra_secreta)):
-                banco_de_palavras_copia[dificuldade].pop(palavra_sorteada)
-                time.sleep(2)
-                os.system('cls')
-                print('='*55)
-                print('VOCÊ GANHOU!!!')
-                print('='*55)
-                time.sleep(3)
-                game_over = True
-            # Verificando derrota
-            elif tentativas <= 0:
-
-                time.sleep(2)
-                os.system('cls')
-                print('='*55)
-                print('VOCÊ FOI ENFORCADO!!!')
-                print('='*55)
-                time.sleep(3)
-                game_over = True
-        # Resposta caso o valor de letra não seja valido
+        print('Escolha a dificildade:')
+        if nivel >= 0:
+            print('1-Facil')
+        if nivel >= 1:
+            print('2-Medio')
+        if nivel >= 2:
+            print('3-Dificil')
+        print('='*55)
+        opcao = input('Escolha: ')
+        if opcao == '1':
+            dificuldade = 'facil'
+            break
+        elif opcao == '2':
+            dificuldade = 'medio'
+            break
+        elif opcao == '3':
+            dificuldade = 'dificil'
+            break
         else:
             os.system('cls')
             print('='*55)
-            print('Digite algo valido!!!')
+            print('Digite um valor valido!!!')
             print('='*55)
-            time.sleep(3)
+            time.sleep(2)
+
+    # Loop reinicio
+    while pontos < 3:
+
+        # Variaveis game
+        game_over = False
+        tentativas = 6
+        alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        palavra = ''
+        acerto = False
+        palavra_secreta = []
+
+        os.system('cls')
+
+        palavra = sorteador_palavra()
+        if palavra == 'game_over':
+            break
+
+        # Criando a palavra_secreta com a palavra
+        for letraP in range(0, len(palavra[1])):
+            if palavra[1][letraP] == ' ':
+                palavra_secreta.append(' ')
+            if palavra[1][letraP] == '-':
+                palavra_secreta.append('-')
+            else:
+                palavra_secreta.append('_')
+
+        # Loop game
+        while game_over == 0:
+            os.system('cls')
+
+            # Tela game
+            print(f'Tentativas: {tentativas}')
+            print(f'Pontos: {pontos}')
+            print(f'Tema: {palavra[0]}')
+            print('='*55)
+            print(str(''.join(palavra_secreta)))
+            print(f'Letras: {len(palavra_secreta)}')
+            print('='*55)
+            print(str(' '.join(alfabeto)))
+            print('='*55)
+            letra = input('Digite uma letra: ')
+
+            # Verificação de validade do valor digitado na variavel letra
+            if len(letra) == 1:
+                acerto = False
+                # Verificando se a letra digitada esta na palavra
+                for a in range(0, len(palavra[1])):
+                    if palavra[1][a] == letra:
+                        # Colocando a letra na palavra_secreta
+                        palavra_secreta[a] = letra
+                        acerto = True
+
+                # Verificando se o usuario acertou a letra
+                if acerto == False and letra in alfabeto:
+                    tentativas -= 1
+
+                # Verificando se a letra esta no alfabeto dispoivel
+                if letra in alfabeto:
+                    # Removendo letra do alfabeto
+                    alfabeto.remove(letra)
+
+                # Verificando vitoria!
+                if palavra[1] == str(''.join(palavra_secreta)):
+                    banco_de_palavras_copia[dificuldade].pop(palavra_sorteada)
+                    pontos += 1
+                    time.sleep(2)
+                    os.system('cls')
+                    print('='*55)
+                    print('VOCÊ GANHOU!!!')
+                    print('='*55)
+                    time.sleep(3)
+                    game_over = True
+                # Verificando derrota
+                elif tentativas <= 0:
+
+                    time.sleep(2)
+                    os.system('cls')
+                    print('='*55)
+                    print('VOCÊ FOI ENFORCADO!!!')
+                    print('='*55)
+                    time.sleep(3)
+                    game_over = True
+            # Resposta caso o valor de letra não seja valido
+            else:
+                os.system('cls')
+                print('='*55)
+                print('Digite algo valido!!!')
+                print('='*55)
+                time.sleep(1.4)
--- teste_2.py
+++ teste_2.py
@@ -4,21 +4,21 @@ import os
 
 # Variavis
 banco_de_palavras = {
-    'facil':{
-        'Sentimento':'amor',
-        'Materia': 'fisica',
-        'Objeto': 'lapis',
-    },
-    'medio':{
-        'Animal': 'tucano',
-        'Pais': 'argentina',
-        'Objeto': 'compasso',
-    },
-    'dificil':{
-        'Animal': 'salamandra ',
-        'Profisão': 'diretor-geral',
-        'Objeto': 'Zarabatana',
-    }
+    'facil': [
+        ('Sentimento', 'amor'),
+        ('Materia', 'fisica'),
+        ('Objeto', 'lapis'),
+    ],
+    'medio': [
+        ('Animal', 'tucano'),
+        ('Pais', 'argentina'),
+        ('Objeto', 'compasso'),
+    ],
+    'dificil': [
+        ('Animal', 'salamandra'),
+        ('Profisão', 'diretor-geral'),
+        ('Objeto', 'Zarabatana'),
+    ]
 }
 
 banco_de_palavras_copia = banco_de_palavras.copy()
@@ -27,8 +27,18 @@ dificuldade = 'facil'
 palavra = ''
 
 
+def sorteador_palavra():
+    global palavra_sorteada, banco_de_palavras_copia
+    palavra_sorteada = random.randint(
+        0, (len(banco_de_palavras_copia[dificuldade]-1)))
+    print(palavra_sorteada)
+    palavra = banco_de_palavras_copia[dificuldade][palavra_sorteada]
+    return palavra
 
 
-banco_de_palavras_copia[dificuldade].pop('amor')
+print(len(banco_de_palavras_copia[dificuldade]))
+print(banco_de_palavras_copia[dificuldade][2])
+# for i in range(0, 5):
+#palavra = sorteador_palavra()
 
-print(banco_de_palavras_copia[dificuldade])
+# print(palavra)
